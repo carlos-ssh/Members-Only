320f5d1a99d80045f3f92a8aa26ed160190a9061
Carlos-SSH <carlos.anezeh@me.com>
2021-01-06 14:38:40 -0600

    Add a profile and validation for dorms.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,13 @@
-class RegistrationsControllers < Devise::RegistrationController
-
+class RegistrationsController < Devise::RegistrationsController 
 
   private
 
-  def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
-  end
+	def sign_up_params 
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+	end
 
-  def account_update_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
-  end
+	def acount_update_params 
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+	end
 
 end
